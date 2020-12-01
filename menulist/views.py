30439b06403e00ad329d84aa375aab3a6d7dfe7d
Seiya Asahina <seiya@asahina>
2020-12-01 14:09:14 +0900

    views.py fixed

--- menulist/views.py
+++ menulist/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render,redirect
 
 from django.views import View
-from .models import Menu,Category,Allergy
+from .models import Menu
 from .forms import MenuForm
 
 class MenuView(View):
 
     def get(self, request, *args, **kwargs):
 
-        cates   = Category.objects.all()
-        alles   = Allergy.objects.all()
         form    = MenuForm()
 
         data    = Menu.objects.order_by("category")
         context = { "data":data,
-                    "form":form,
-                    "cates":cates,
-                    "alles":alles }
+                    "form":form}
 
         return render(request,"menulist/index.html",context)
     
@@ -27,10 +23,8 @@ class MenuView(View):
         if form.is_valid():
             print("バリデーションOK")
             form.save()
-            
         else:
             print("バリデーションNG")
-
 
 
         return redirect("menulist:index")
